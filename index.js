0c8f1aa05856ef77d00ff5ece046d40464062fef
Raymond <oetaray@gmail.com>
2023-06-22 20:57:19 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your solution in this file!
+let employee = {
+    name: "Raymond",
+    streetAddress: "Mamlaka RD, Kilimani"
+
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    let updatedEmployee={...employee};
+    return updatedEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key]
+    return employee;
+}
